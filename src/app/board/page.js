324a5dcc96f2441df2c7307a013e69c174cd0d49
Jacob Meneses <jaykobyou@gmail.com>
2024-05-20 18:05:31 -0600

    Adding drag and drop example

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,5 +1,40 @@
-import styles from "./page.module.css";
+'use client';
 
+import React, { useState } from 'react';
+import styles from "./page.module.css";
+import { DndContext, useDroppable, useDraggable } from '@dnd-kit/core';
+
+function Draggable(props) {
+    const { id, v } = props;
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+      id: id,
+    });
+    const style = transform ? {
+      transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+    } : undefined;
+
+    return (
+      <div key={id} ref={setNodeRef} className={styles.task} style={style} {...listeners} {...attributes}><p key={id}>{v.title}</p></div>
+    );
+}
+
+
+function Droppable(props) {
+    const { title } = props;
+    const {isOver, setNodeRef} = useDroppable({
+      id: props.id,
+    });
+    const style = {
+      color: isOver ? 'green' : undefined,
+    };
+
+    return (
+      <div className={styles.stack} ref={setNodeRef} style={style}>
+        <div className={styles.center}><p>{title}</p></div>
+        {props.children}
+      </div>
+    );
+  }
 
 export default function Board() {
     const stack_title = 'TO DO';
@@ -15,16 +50,57 @@ export default function Board() {
         },
     ];
 
+    const [ stack1, setStack1 ] = useState([])
+    const [ stack2, setStack2 ] = useState([])
+
+    function handleDragEnd(event) {
+        const { active, over } = event;
+        const id_splitted = active.id.split('-');
+        const source = id_splitted[1];
+        const index = parseInt(id_splitted[0], 10);
+
+        console.log(event);
+
+        let fn, stack;
+
+        if ( source !== 'middle') {
+            console.log('Source not valid for now');
+            return;
+        }
+
+        const item = tasks[index];
+
+        switch (over.id) {
+            case 'droppable': fn = setStack1; stack = stack1; break;
+            case 'container': fn = setStack2; stack = stack2; break;
+        }
+
+        fn((prev) => {
+            const new_list = Object.assign([], prev);
+
+            new_list.push(item);
+
+            return new_list;
+        }, [])
+    }
+
     return (
-        <main>
-            <div className={styles.grid}>
-                <div className={styles.stack}>
-                    <div className={styles.center}><p>{stack_title}</p></div>
-                    { tasks.map((v, i) => (<div className={styles.task}><p key={i}>{v.title}</p></div>)) }
+        <DndContext onDragEnd={handleDragEnd}>
+            <main>
+                <div className={styles.grid}>
+                    <Droppable id="droppable" title={stack_title}>
+                        { stack1.map((v, i) => (<Draggable key={i + '-stack1'} id={i + '-stack1'} v={v} />)) }
+                    </Droppable>
+
+                    <div className={styles.stack}>
+                        { tasks.map((v, i) => (<Draggable key={i + '-middle'} id={i + '-middle'} v={v} />)) }
+                    </div>
+
+                    <Droppable id="container" title="demo">
+                    { stack2.map((v, i) => (<Draggable key={i + '-demo'} id={i + '-demo'} v={v} />)) }
+                    </Droppable>
                 </div>
-                <div className={styles.stack}><p>Task 2</p></div>
-                <div className={styles.stack}><p>Task 3</p></div>
-            </div>
-        </main>
+            </main>
+        </DndContext>
     );
 }
