3a7a366e1ffd273aec84cab38570e7ffdb8d221f
Jacob Meneses <jaykobyou@gmail.com>
2024-06-18 08:40:58 -0600

    Function delete at backlog

--- src/app/backlog/page.js
+++ src/app/backlog/page.js
@@ -1,12 +1,14 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import styles from "./page.module.css";
-import { fetchTasks } from '../board/fetchTasks'
+import { fetchTasks, deleteTask } from '../board/fetchTasks'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
+
 export default function Backlog() {
     const [ tasks, setTasks ] = useState([]);
+    const [ deleteIndex, setDeleteIndex ] = useState(-1)
 
     useEffect(() => {
         const fn = async () => {
@@ -22,18 +24,44 @@ export default function Backlog() {
         fn();
     }, [])
 
-    const onClickDelete = function(e) {
+    useEffect(() => {
+        const fn = async () => {
+            if ( deleteIndex === -1 ) {
+                return;
+            }
 
+            try {
+                await deleteTask({
+                    taskId: tasks[deleteIndex].id
+                })
+
+                setTasks(prev => {
+                    const next = prev.map(v => Object.assign({}, v));
+
+                    next.splice(deleteIndex, 1);
+
+                    return next;
+                })
+            } catch (e) {
+                console.log(e);
+            }
+        };
+
+        fn();
+    }, [ deleteIndex ])
+
+    const onClickDelete = function(index) {
+        setDeleteIndex(index);
     };
 
     return (
         <main>
             <div className={styles.stack}>
-                { tasks.map((v) => {
+                { tasks.map((v, index) => {
                     return  (<div key={v.id} className={styles.task}>
                          <FontAwesomeIcon onClick={(e) => {
                             e.stopPropagation()
-                            onClickDelete(e)
+                            onClickDelete(index)
                         }} className={styles.xmarkIcon} icon={faXmark} />
                         <div className={styles.taskInner} >
                             <p key={v.id}>{v.title}</p>
